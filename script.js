b4041dfbb9316404cfe3d526f448d0f52f7d01f1
Victoria Kee <victoriakee95@gmail.com>
2020-06-13 15:15:45 -0500

    Write main function

--- script.js
+++ script.js
@@ -51,3 +51,36 @@ const questions = [{
 let currentQuestion = 0;
 let correctAnswers = 0;
 let quizOver = false;
+
+$(document).ready(function(){
+  displayCurrentQuestion();
+  $(this).find('.quizMessage').hide();
+  $(this).find('.nextButton').on('click', function() {
+    if (!quizOver) {
+      value = $("input[type='radio']:checked").val();
+      if (value == undefined) {
+        $(document).find('.quizMessage').text("Please select an answer.");
+        $(document).find('.quizMessage').show();
+      } else {
+        $(document).find('.quizMessage').hide();
+        if (value == questions[currentQuestion].correctAnswer) {
+          correctAnswers++;
+        }
+        currentQuestion++;
+        if (currentQuestion < questions.length) {
+          displayCurrentQuestion();
+        } else {
+          displayScore();
+          $(document).find('.nextButton').text('Play Again?');
+          quizOver = true;
+        }
+      }
+    } else {
+      quizOver = false;
+      $(document).find('.nextButton').text('Next Question');
+      resetQuiz();
+      displayCurrentQuestion();
+      hideScore();
+    }
+  });
+});
