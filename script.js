71a0fee867ff702524e61c2286ddd9086b751a74
Victoria Kee <victoriakee95@gmail.com>
2020-06-13 14:57:21 -0500

    Add quiz questions

---
+++ script.js
@@ -0,0 +1,49 @@
+const questions = [{
+      question: "What is the baby of a moth known as?",
+      choices: ['baby', 'infant', 'kit', 'larva'],
+      correctAnswer: 3
+}, {
+      question: "What is the adult of a kid called?",
+      choices: ['baby', 'doe', 'goat', 'chick'],
+      correctAnswer: 2
+}, {
+      question: "What are the young of buffalo?",
+      choices: ['calf', 'baby', 'pup', 'cow'],
+      correctAnswer: 0
+}, {
+      question: "What is a baby alligator called?",
+      choices: ['baby', 'gator', 'hatchling', 'calf'],
+      correctAnswer: 2
+}, {
+      question: "What is a baby goose called?",
+      choices: ['gooser', 'gosling', 'gup', 'pup'],
+      correctAnswer: 1
+}, {
+      question: "What a baby hamster called?",
+      choices: ['pup', 'chick', 'infant', 'billy'],
+      correctAnswer: 0
+}, {
+      question: "What is a baby hawk called?",
+      choices: ['hawklett', 'pup', 'larva', 'eyas'],
+      correctAnswer: 3
+}, {
+      question: "What is a baby grasshopper called?",
+      choices: ['hopper', 'nymph', 'stick', 'grassie'],
+      correctAnswer: 1
+}, {
+      question: "What is a baby kangaroo called?",
+      choices: ['kinga', 'joey', 'calf', 'baby'],
+      correctAnswer: 1
+}, {
+      question: "What is a baby whale called?",
+      choices: ['whala', 'cub', 'grub', 'infant'],
+      correctAnswer: 1
+}, {
+      question: "What is a baby monkey called?",
+      choices: ['infant', 'baby', 'calf', 'pup'],
+      correctAnswer: 0
+}, {
+      question: "What is a baby bear called?",
+      choices: ['cub', 'balu', 'young', 'bearlet'],
+      correctAnswer: 0
+}];
